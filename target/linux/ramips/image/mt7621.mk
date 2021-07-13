ad6d3942a643a7431c80939146a68463a24f5d78
Tonkercke <67683757+Tonkercke@users.noreply.github.com>
2021-07-13 10:44:31 +0900

    Update mt7621.mk

--- target/linux/ramips/image/mt7621.mk
+++ target/linux/ramips/image/mt7621.mk
@@ -312,7 +312,7 @@ TARGET_DEVICES += xiaomi_mir3g
 
 define Device/xiaomi_mir3s
   DTS := MIR3S
-  IMAGE_SIZE := $(ralink_default_fw_size_16M)
+  IMAGE_SIZE := $(ralink_default_fw_size_32M)
   DEVICE_TITLE := Xiaomi Mir3S
   DEVICE_PACKAGES := kmod-mt7603 kmod-mt76x2 kmod-usb3 kmod-usb-ledtrig-usbport wpad-basic
 endef
