d60b3c8cd689d92dbb9f78565485c49b53a8f81d
Tonkercke <another2468@protonmail.com>
2022-03-03 10:23:11 +0900

    update

--- target/linux/ramips/image/mt7620.mk
+++ target/linux/ramips/image/mt7620.mk
@@ -818,7 +818,7 @@ TARGET_DEVICES += ohyeah_oy-0001
 
 define Device/phicomm_k2g
   SOC := mt7620a
-  IMAGE_SIZE := 7552k
+  IMAGE_SIZE := 16064k
   DEVICE_VENDOR := Phicomm
   DEVICE_MODEL := K2G
   DEVICE_PACKAGES := kmod-mt76x2
