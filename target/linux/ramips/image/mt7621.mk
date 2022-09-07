3e050e0c905b218402de5139b4a662642e71e0a1
Tonkercke <another2468@protonmail.com>
2022-09-07 21:14:17 +0900

    update 0907

--- target/linux/ramips/image/mt7621.mk
+++ target/linux/ramips/image/mt7621.mk
@@ -1476,7 +1476,7 @@ define Device/phicomm_k2p
   DEVICE_ALT0_VENDOR := Phicomm
   DEVICE_ALT0_MODEL := KE 2P
   SUPPORTED_DEVICES += k2p
-  DEVICE_PACKAGES := kmod-mt7615e kmod-mt7615-firmware
+  DEVICE_PACKAGES := kmod-mt7615d kmod-mt7615-firmware
 endef
 TARGET_DEVICES += phicomm_k2p
 
